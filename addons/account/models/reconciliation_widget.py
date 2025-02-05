62e9fb1bbb18c426c8f6f9e9f94107d80705c452
Irawan <77831036+irawan-w6@users.noreply.github.com>
2025-02-05 16:22:37 +0700

    Update reconciliation_widget.py remove order by date_maturity (performance issue)

--- addons/account/models/reconciliation_widget.py
+++ addons/account/models/reconciliation_widget.py
@@ -283,7 +283,7 @@ class AccountReconciliation(models.AbstractModel):
 
         domain = self._domain_move_lines_for_manual_reconciliation(account_id, partner_id, excluded_ids, search_str)
         # recs_count = Account_move_line.search_count(domain)
-        lines = Account_move_line.search(domain, limit=limit, order="date_maturity desc, id desc")
+        lines = Account_move_line.search(domain, limit=limit)
         if target_currency_id:
             target_currency = Currency.browse(target_currency_id)
         else:
